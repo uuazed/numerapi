662d9a6dc87f34ce8c14bb6a544e4fec2cebefff
uuazed <uuazed@gmail.com>
2020-05-27 22:00:10 +0200

    test for quant api

--- tests/test_quantapi.py
+++ tests/test_quantapi.py
@@ -36,3 +36,44 @@ def test_upload_predictions(api, tmpdir):
     submission_id = api.upload_predictions(str(path))
     assert submission_id == "1234"
     assert len(responses.calls) == 3
+
+
+@responses.activate
+def test_daily_submissions_performances(api):
+    perf = {'date': "20200516",
+            'returns': 1.256,
+            'submissionTime': "20200516"}
+    data = {'data': {'quantUserProfile':
+            {'dailySubmissionPerformances': [perf]}}}
+    responses.add(responses.POST, base_api.API_TOURNAMENT_URL, json=data)
+    result = api.daily_submissions_performances("uuazed")
+    assert len(result) == 1
+    assert isinstance(result[0]["date"], datetime.datetime)
+    assert isinstance(result[0]["submissionTime"], datetime.datetime)
+
+
+@responses.activate
+def test_daily_user_performances(api):
+    perf = {'rank': 12,
+            'sharpe': 1.256,
+            'date': "20200516"}
+    data = {'data': {'quantUserProfile':
+            {'dailyUserPerformances': [perf]}}}
+    responses.add(responses.POST, base_api.API_TOURNAMENT_URL, json=data)
+    result = api.daily_user_performances("uuazed")
+    assert len(result) == 1
+    assert isinstance(result[0]["date"], datetime.datetime)
+
+
+@responses.activate
+def test_public_user_profile(api):
+    profile = {'rank': 12,
+               'id': "asdfas",
+               'username': "uuazed",
+               'bio': "bio",
+               'sharpe': 1.256,
+               'startDate': "20200516"}
+    data = {'data': {'quantUserProfile': profile}}
+    responses.add(responses.POST, base_api.API_TOURNAMENT_URL, json=data)
+    result = api.public_user_profile("uuazed")
+    assert isinstance(result["startDate"], datetime.datetime)
