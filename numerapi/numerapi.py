167690a0f22a98ea400271b01a5c98bc9f022add
uuazed <uuazed@gmail.com>
2021-03-18 22:34:15 +0100

    codestyle

--- numerapi/numerapi.py
+++ numerapi/numerapi.py
@@ -715,8 +715,8 @@ class NumerAPI(base_api.Api):
     def upload_predictions(self, file_path: str, tournament: int = 8,
                            model_id: str = None) -> str:
         """Upload predictions from file.
-        Will read TRIGGER_ID from the environment if this model is enabled with a
-        Numerai Compute cluster setup by Numerai CLI.
+        Will read TRIGGER_ID from the environment if this model is enabled with
+        a Numerai Compute cluster setup by Numerai CLI.
 
         Args:
             file_path (str): CSV file with predictions that will get uploaded
