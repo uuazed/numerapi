507d7786659e04ffc5c97898c58975bf6e209fc0
Aditya <adityang5@gmail.com>
2021-03-20 13:26:39 +0530

    Resumable Download (#42)
    * Resumable Download
    
    * Resumable Download changes

--- numerapi/utils.py
+++ numerapi/utils.py
@@ -34,17 +34,43 @@ def replace(dictionary: Dict, key: str, function):
 
 
 def download_file(url: str, dest_path: str, show_progress_bars: bool = True):
-    r = requests.get(url, stream=True)
-    r.raise_for_status()
+    file_size = 0
+    req = requests.get(url, stream=True)
+    req.raise_for_status()
+
     # Total size in bytes.
-    total_size = int(r.headers.get('content-length', 0))
+    total_size = int(req.headers.get('content-length', 0))
+
+    if os.path.exists(dest_path):
+        logger.info("target file already exists")
+        file_size = os.stat(dest_path).st_size  # File size in bytes
+        if file_size < total_size:
+            # Download incomplete
+            logger.info("resuming download")
+            resume_header = {'Range': 'bytes=%d-' % file_size}
+            req = requests.get(url, headers=resume_header, stream=True,
+                               verify=False, allow_redirects=True)
+        elif file_size == total_size:
+            # Download complete
+            logger.info("download complete")
+            return
+        else:
+            # Error, delete file and restart download
+            logger.error("deleting file and restarting")
+            os.remove(dest_path)
+            file_size = 0
+    else:
+        # File does not exist, starting download
+        logger.info("starting download")
 
     # write dataset to file and show progress bar
     pbar = tqdm.tqdm(total=total_size, unit='B', unit_scale=True,
                      desc=dest_path, disable=not show_progress_bars)
-    with open(dest_path, "wb") as f:
-        for chunk in r.iter_content(1024):
-            f.write(chunk)
+    # Update progress bar to reflect how much of the file is already downloaded
+    pbar.update(file_size)
+    with open(dest_path, "ab") as dest_file:
+        for chunk in req.iter_content(1024):
+            dest_file.write(chunk)
             pbar.update(1024)
 
 
