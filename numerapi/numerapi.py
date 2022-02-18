cddeb1e2d4383b3bab1824e4a14b69ed1a1cbb5b
Nicholas Pezolano <npezolano@gmail.com>
2022-02-18 13:07:24 -0500

    add multi-model stake_set support for NumerAPI #70 (#71)
    * add multi-model stake_set support for NumerAPI
    
    * rename param and make model_id requried  #70
    
    * rename username to model_id #70

--- numerapi/numerapi.py
+++ numerapi/numerapi.py
@@ -734,11 +734,12 @@ class NumerAPI(base_api.Api):
             utils.replace(item, "nmrStaked", utils.parse_float_string)
         return data
 
-    def stake_set(self, nmr) -> Dict:
+    def stake_set(self, nmr, model_id) -> Dict:
         """Set stake to value by decreasing or increasing your current stake
 
         Args:
             nmr (float or str): amount of NMR you want to stake
+            model_id (str): model_id for where you want to stake
 
         Returns:
             dict: stake information with the following content:
@@ -764,10 +765,8 @@ class NumerAPI(base_api.Api):
              'posted': True,
              'value': '10'}
         """
-        # get username of logged in user
-        username = self.get_account()['username']
         # fetch current stake
-        current = self.stake_get(username)
+        current = self.stake_get(model_id)
         # convert everything to decimals
         if current is None:
             current = decimal.Decimal(0)
@@ -777,9 +776,9 @@ class NumerAPI(base_api.Api):
             nmr = decimal.Decimal(str(nmr))
         # update stake!
         if nmr < current:
-            return self.stake_decrease(current - nmr)
+            return self.stake_decrease(current - nmr, model_id)
         if nmr > current:
-            return self.stake_increase(nmr - current)
+            return self.stake_increase(nmr - current, model_id)
         self.logger.info("Stake already at desired value. Nothing to do.")
         return None
 
