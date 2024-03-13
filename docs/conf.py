e2d568688ea8c37971801f854418fc4a830e15b6
uuazed <34725120+uuazed@users.noreply.github.com>
2024-03-13 22:52:55 +0100

    Readthedocsfix2 (#108)
    * try
    
    * adding m2r

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
+    'm2r'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
